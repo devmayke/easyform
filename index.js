b04fbba2a957efb6b6cece28dcfac4f5589a483d
Mayke Ken <devmayke@gmail.com>
2021-12-03 13:55:51 -0300

    .

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const app = express()
 const router = require("./router")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 let PORT = 7000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(cors())
 app.use("/", router)
 
 app.listen(process.env.PORT || PORT, ()=>console.log("conectado na porta em http://localhost:"+ PORT))
